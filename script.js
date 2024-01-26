fe1fa317f05fe4a6651972df37c38ec54c84d3df
Semion Rudman <SemionR@VoltaBelting.com>
2024-01-26 21:36:18 +0200

    Fri 01-26-24

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+var currentState = 0;
+var actionState = null;
+var leftOperand = 0;
+var rightOperand = 0;
+
+const buttonHandlerNumber = (button) => {
+    const buttonNumberValue = button.textContent.valueOf();
+    var displayValue = 0;
+    if(actionState == null) {
+        displayValue = Number(buttonNumberValue) + Number(leftOperand) * 10;
+        leftOperand = displayValue;
+    } else {
+        displayValue = Number(buttonNumberValue) + Number(rightOperand) * 10;
+        rightOperand = displayValue;
+        
+    }
+    updateDisplayLower(displayValue.valueOf());
+}
+
+const buttonHandlerBasicAction = (button, actionString) => {
+    if (actionState == null){
+        actionState = actionString;
+        updateDisplayUpper(leftOperand, button)
+        updateDisplayLower('')
+    } else {
+        return;
+    }
+}
+
+const updateDisplayLower = (value) => {
+    const display = document.getElementById("display-lower");
+    display.textContent = value.toString();
+}
+
+const updateDisplayUpper = (value, button) => {
+    const display = document.getElementById("display-upper");
+    display.textContent = display.textContent + value + " " + button.textContent + " "
+}
+
+
 // custom config for the MTW app 
 const mtwAppConfig = {
     border: true // app border in the MTW page                    
