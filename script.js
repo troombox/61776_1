a119a91ef71bbe87baf4e7a04cbf0d6737173379
Semion Rudman <SemionR@VoltaBelting.com>
2024-01-26 18:58:17 +0200

    Fri 01-26-24

---
+++ script.js
@@ -0,0 +1,22 @@
+// custom config for the MTW app 
+const mtwAppConfig = {
+    border: true // app border in the MTW page                    
+}
+
+///////////////////////////////////////////////////////
+// implement this functions in order to use private API        
+const sendPrivateApiRequest = async () => {
+    // your code here:
+    // sendRequestToParent({ cat: 'getAssets' })
+}        
+
+const acceptPrivateApiResponse = async (data) => {
+    // process received data here:
+    // console.log(data)
+}
+
+// use this in order to send on document load
+document.addEventListener('DOMContentLoaded', () => {
+    // your code here:
+    //sendRequestToParent({ method: 'getAssets' })
+})
